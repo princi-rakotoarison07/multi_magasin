bfe48fa831cae0786d55d188c639df3fa02a4097
Princi Rakotoarison <princirakotoarison07@gmail.com>
2026-02-17 19:36:56 +0300

    Page d'accueil  , home

---
+++ src/main/java/com/magasin/multi_magasin/HomeController.java
@@ -0,0 +1,13 @@
+package com.magasin.multi_magasin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
